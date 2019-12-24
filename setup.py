f55dc8c03c1be24dce42b77638bee3a7fc83003a
Michael Mohr <akihana@gmail.com>
2019-12-23 20:27:11 -0800

    Hack up setup.py to build assembly-optimized code

--- setup.py
+++ setup.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python3
 
 import platform
+import subprocess
+import distutils
+from distutils.cmd import Command
 from distutils.core import setup, Extension
+from distutils.command.build_ext import build_ext
+
+# https://jichu4n.com/posts/how-to-add-custom-build-steps-and-commands-to-setuppy/
+class YasmCommand(Command):
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        command = ["yasm", "-D__GNUC__", "-g", "dwarf2", "-f", "elf64", "../aes/aes_amd64.asm"]
+        self.announce("Running command: {}".format(" ".join(command)))
+        subprocess.check_call(command)
+
+
+class BuildExtCommand(build_ext):
+  def run(self):
+    self.run_command("yasm")
+    super().run()
 
 source_files = [
     "aesmodule.c",
@@ -18,13 +41,29 @@ if platform.system() == "Linux":
 
 if platform.machine() == "x86_64":
     source_files.append("../aes/aes_ni.c")
+    define_macros = [("ASM_AMD64_C", None)]
+    cmdclass = {
+        "yasm": YasmCommand,
+        "build_ext": BuildExtCommand,
+    }
+    extra_objects = ["aes_amd64.o"]
+else:
+    cmdclass = {}
+    extra_objects = []
+    define_macros = []
 
 setup(
+    cmdclass=cmdclass,
     name="aes",
     version="1.0",
     ext_modules=[
         Extension(
-            "aes", source_files, include_dirs=["../aes"], extra_compile_args=cflags
+            name="aes",
+            sources=source_files,
+            include_dirs=["../aes"],
+            define_macros=define_macros,
+            extra_compile_args=cflags,
+            extra_objects=extra_objects,
         )
     ],
 )
